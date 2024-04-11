d45c1efb998a73dca7b8b618579686136d0e2155
iamrajeev2005 <fea.rajeev@gmail.com>
2024-04-11 23:02:16 +0530

    marquee animation done

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
-function Marquee({url}) {
+function Marquee({ url,direction }) {
   return (
-    <div className='flex items-center gap-10'>
-      {url.map((item, index) => {
-        return <img key={index} src={item} alt="" />;
-      })}
+    <div className="w-full flex overflow-hidden items-center gap-16">
+      <motion.div
+        initial={{ x:direction === "left" ? "0": "-100%" }}
+        animate={{ x: direction === "left" ? "-100%" : "0" }}
+        transition={{ ease: "linear", duration: 16, repeat: Infinity }}
+        className="flex flex-shrink-0 items-center gap-10"
+      >
+        {url.map((item, index) => {
+          return <img key={index} src={item} alt="" />;
+        })}
+      </motion.div>
+      <motion.div
+        initial={{ x:direction === "left" ? "0": "-100%" }}
+        animate={{ x: direction === "left" ? "-100%" : "0" }}
+        transition={{ ease: "linear", duration: 16, repeat: Infinity }}
+        className="flex flex-shrink-0 items-center gap-10"
+      >
+        {url.map((item, index) => {
+          return <img key={index} src={item} alt="" />;
+        })}
+      </motion.div>
     </div>
   );
 }
 
-export default Marquee
+export default Marquee;
